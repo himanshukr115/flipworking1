1bea4f96c8a570201c3e272c1261d35c998ddbc6
unknown <himanshukr115@gmail.com>
2024-06-21 20:48:50 +0530

    firts comment 7

--- src/UPIPayment.jsx
+++ src/UPIPayment.jsx
@@ -11,13 +11,13 @@ const UPIPayment = () => {
     }
   }, []); // Empty dependency array means this runs once after initial render
 
-  const upiId = "himanshukr1151@axl";
-  const payeeName = "Himanshu Kumar";
-  const transactionNote = "Payment for Payee";
-
-  // const upiId = "9099681841@hdfcbank";
-  // const payeeName = "Ganesh Pharma";
+  // const upiId = "himanshukr1151@axl";
+  // const payeeName = "Himanshu Kumar";
   // const transactionNote = "Payment for Payee";
+
+  const upiId = "9099681841@hdfcbank";
+  const payeeName = "Ganesh Pharma";
+  const transactionNote = "Payment for Payee";
 
   const initiateUPIPayment = () => {
     const upiLink = `upi://pay?pa=${encodeURIComponent(upiId)}&pn=${encodeURIComponent(payeeName)}&am=${encodeURIComponent(amtToBePaid)}&tn=${encodeURIComponent(transactionNote)}&cu=INR`;
